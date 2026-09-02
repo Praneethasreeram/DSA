52ac61b4c5810ef50ccaaae2be95ae7eb465e28b
Praneetha Sreeram <24wh1a0431@bvrithyderabad.edu.in>
2026-09-02 11:38:11 +0530

    Time: 6 ms (47.89%) | Memory: 46.4 MB (98.83%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int array[]=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            int diff=target-nums[i];
+            if(map.containsKey(diff))
+            {
+                array[0]=i;
+                array[1]=map.get(diff);
+            }
+            else
+            {
+                map.put(nums[i],i);
+            }
+            
+            
+        }
+        
+        return array;
+    }
+}
