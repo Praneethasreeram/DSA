8293b5a62f807805a1198d1428112694ae4eef35
Praneetha Sreeram <24wh1a0431@bvrithyderabad.edu.in>
2026-09-02 11:48:12 +0530

    Time: 49 ms (5.13%) | Memory: 102.9 MB (74.98%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        boolean res=false;
+        for(int i=0;i<nums.length;i++)
+        {
+             if(!map.containsKey(nums[i]))
+             {
+                map.put(nums[i],1);
+             }
+             else
+             {
+                map.put(nums[i],map.get(nums[i])+1);
+             }
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(map.get(nums[i])>=2)
+            {
+                res=true;
+                break;
+            }
+
+        }
+        return res;
+    }
+}
