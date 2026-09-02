f5363ef4dbba9a850e8f1238f711d762d81d9755
Praneetha Sreeram <24wh1a0431@bvrithyderabad.edu.in>
2026-09-02 13:42:58 +0530

    Time: 2 ms (15.53%) | Memory: 43.7 MB (8.84%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+        
+        
+    }
+}
