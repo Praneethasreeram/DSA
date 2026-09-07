a3782d342579cb8b6aea0ef0fe384ae4b3489b6f
Praneetha Sreeram <24wh1a0431@bvrithyderabad.edu.in>
2026-09-07 14:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (58.81%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -9,8 +9,8 @@ class Solution {
         {
             dp[0]=0;
             dp[1]=0;
-            dp[2]=Math.min(cost[0],cost[1]);
-            for(int i=3;i<dp.length;i++)
+           // dp[2]=Math.min(cost[0],cost[1]);
+            for(int i=2;i<dp.length;i++)
             {
                 dp[i]=Math.min(cost[i-2]+dp[i-2],cost[i-1]+dp[i-1]);
             }
