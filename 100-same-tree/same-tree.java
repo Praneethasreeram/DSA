e445c3d0c9d26eddc224777d759120c4467cada9
Praneetha Sreeram <24wh1a0431@bvrithyderabad.edu.in>
2026-09-23 14:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (70.60%) - LeetSync

---
+++ 100-same-tree/same-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   //boolean res=true;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+        {
+            return true;
+        }
+        else if(p==null && q!=null)
+        {
+            return false;
+        }
+        else if(p!=null && q==null)
+        {
+            return false;
+        }
+        if(p.val==q.val)
+        {
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }
+        else
+        {
+            return false;
+        }
+
+
+    }
+}
